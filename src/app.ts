2a8ef27eb639a2b935646796db73c19d0ab1dac3
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-14 23:50:00 +0530

    feat: rendering a map with openlayers with dummy coordinates

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-
-const GOOGLE_API_KEY = "AIzaSyDbl0X0Da9iZxX9yevWDbqCkvRonNY2UcQ";
+declare var ol: any;
 
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address") as HTMLInputElement;
@@ -14,32 +12,22 @@ function searchAddressesHandler(event: Event) {
   event.preventDefault();
 
   const enteredAddress = addressInput.value;
-  const API_ENDPOINT = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-    enteredAddress
-  )}&key=${GOOGLE_API_KEY}`;
+  const coordinates = { lat: 40.41, lng: -73.99 }; // Can't fetch coordinates from Google API, use dummy ones
 
-  axios
-    .get<GoogleGeocodingResponse>(API_ENDPOINT)
-    .then((response) => {
-      if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location!");
-      }
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
 
-      const coordinates = response.data.results[0].geometry.location;
-      const map = new google.maps.Map(
-        document.getElementById("map") as HTMLElement,
-        {
-          center: coordinates,
-          zoom: 16,
-        }
-      );
-
-      new google.maps.Marker({ position: coordinates, map });
-    })
-    .catch((err) => {
-      alert(err.message);
-      console.log(err);
-    });
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 12,
+    }),
+  });
 }
 
 form.addEventListener("submit", searchAddressesHandler);
