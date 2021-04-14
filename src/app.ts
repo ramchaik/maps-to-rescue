fba31d289860aed94ada46bacc89bbf1a57a59cc
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-14 22:52:24 +0530

    feat: get user input

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("what's up!");
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+function searchAddressesHandler(event: Event) {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  //TODO: send this to Google API
+}
+
+form.addEventListener("submit", searchAddressesHandler);
