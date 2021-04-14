5fe80d04923344a54377c9c81574b43c07a02d1e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-04-14 23:36:16 +0530

    feat: fetch coordinates for entered address

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,37 @@
-const form = document.querySelector("form")!;
+import axios from "axios";
+
+const GOOGLE_API_KEY = "AIzaSyDbl0X0Da9iZxX9yevWDbqCkvRonNY2UcQ";
+
+const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address") as HTMLInputElement;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressesHandler(event: Event) {
   event.preventDefault();
 
   const enteredAddress = addressInput.value;
 
-  //TODO: send this to Google API
+  const API_ENDPOINT = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    enteredAddress
+  )}&key=${GOOGLE_API_KEY}`;
+  axios
+    .get<GoogleGeocodingResponse>(API_ENDPOINT)
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener("submit", searchAddressesHandler);
